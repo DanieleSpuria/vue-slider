c563aee0b791c9fe6a9ec2eb3078540271636549
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-13 18:06:26 +0200

    add time()

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,27 @@ createApp({
     prev(){
       if (this.click === 1) this.click = 5;
       else this.click--;
-      console.log('click', this.click);
     },
 
     next(){
       if (this.click === 5) this.click = 1;
       else this.click++;
-      console.log('click', this.click);
     },
 
     on(i) {
       this.click = i;
-      console.log(i);
+    },
+
+    callTime() {
+      this.click++
+    },
+
+    time() {
+      setInterval(this.callTime, 3000)
     }
+  },
+
+  mounted() {
+    this.time()
   }
 }).mount('#app')
