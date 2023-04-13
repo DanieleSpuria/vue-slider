a7a83061af6436f81296585723bbbb347bf9ad3c
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-13 16:37:50 +0200

    add img

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ createApp({
   data(){
     return{
       counter: 5,
+      img: [
+        'assets/img/01.webp',
+        'assets/img/02.webp',
+        'assets/img/03.webp',
+        'assets/img/04.webp',
+        'assets/img/05.webp'
+      ]
     }
   },
 
